da0f74785cedd6d5958213bbcedcd35e13ffa21b
AsterNighT <asternight@zju.edu.cn>
2018-11-23 08:53:28 +0800

    Implement a simple menu

--- FIAR/menu.h
+++ FIAR/menu.h
@@ -1,3 +1,8 @@
 #pragma once
+#include <stdio.h>
+#define clear() system("cls")
+#define pause() system("pause")
 int displayMainMenu();
+int displayDeveloperInfo();
+int displayTutorial();
 int displaySettingMenu();
