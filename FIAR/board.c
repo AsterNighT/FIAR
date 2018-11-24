7d3441b425bb8891ee57ec2107353a370c8870f2
AsterNighT <asternight@zju.edu.cn>
2018-11-24 19:57:48 +0800

    Progess on board and control flow

--- FIAR/board.c
+++ FIAR/board.c
@@ -1,5 +1,6 @@
 #pragma once
 #include "board.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 // Class for game board
@@ -8,7 +9,7 @@ struct Board* newBoard() {
     struct Board* board = malloc(sizeof(struct Board));
     memset(board->board, 0, sizeof(board->board));
     board->currentPlayer = 1;
-    board->gameStatus    = 0;
+    board->gameStatus    = 1;
     board->movesCount    = 0;
     return board;
 }
@@ -21,14 +22,15 @@ int placePieceBoard(struct Board* board, int cordx, int cordy) {
     board->moves[board->movesCount].player = board->currentPlayer;
     board->movesCount++;
     board->currentPlayer = 1 + board->currentPlayer & 1;
-    return checkStatusBoard(board);
+    board->gameStatus    = checkStatusBoard(board);
+    return board->gameStatus;
 }
 
 struct CharPair encodeMove(struct Move move) {
     struct CharPair ret;
-    int value = (move.cordx * 16 + move.cordy) + (move.player - 1) * 300;
-    ret.fst   = 'a' + value / 26;
-    ret.snd   = 'a' + value % 26;
+    int             value = (move.cordx * 16 + move.cordy) + (move.player - 1) * 300;
+    ret.fst               = 'a' + value / 26;
+    ret.snd               = 'a' + value % 26;
     return ret;
 }
 
@@ -42,14 +44,15 @@ struct Move decodeMove(struct CharPair code) {
 }
 
 // Remember to free the memory afterwards!!!!!!!!!
-char* saveReplayBoard(struct Board* board) {
-    char* ret = malloc(512 * sizeof(char));
+int saveReplayBoard(struct Board* board) {
+    char code[512]= {};
     for (int index = 0; index < board->movesCount; index++) {
         struct CharPair encodedMove = encodeMove(board->moves[index]);
-        ret[index * 2]              = encodedMove.fst;
-        ret[index * 2 + 1]          = encodedMove.snd;
+        code[index * 2]              = encodedMove.fst;
+        code[index * 2 + 1]          = encodedMove.snd;
     }
-    return ret;
+    printf("%s\n",code);
+    return 0;
 }
 
 int playReplayBoard(struct Board* board, char* replayData) {
@@ -58,8 +61,40 @@ int playReplayBoard(struct Board* board, char* replayData) {
     for (int index = 0; index < length; index += 2) {
         struct CharPair code     = {replayData[index], replayData[index + 1]};
         struct Move     moveTemp = decodeMove(code);
-        int ret = placePieceBoard(board, moveTemp.cordx, moveTemp.cordy);
+        int             ret      = placePieceBoard(board, moveTemp.cordx, moveTemp.cordy);
         if (ret != 1 && ret != 2) return 1;
+        displayBoard(board);
     }
     return 0;
+}
+int startGameBoard(struct Board* board, int type) {
+    while (1) {
+        displayBoard(board);
+        int param1, param2;
+        scanf_s("%d", &param1);
+        if (param1 > 16) {
+            switch (param1) {
+                case 17: saveBoard(board); break;
+                case 18:
+                    char* data;
+                    scanf("%s", data);
+                    loadBoard(board, data);
+                    break;
+                case 19: saveReplayBoard(board); break;
+                case 20: return 0;
+            }
+        } else {
+            if (board->gameStatus == 1 && board->gameStatus == 2) {
+                scanf_s("%d", param2);
+                int status = placePieceBoard(board, param1, param2);
+                if (status == 0) printf("Invalid Move.\n");
+                if (status < 0) printf("Player %d wins!\n", status - 1);
+                if (status == 3) printf("It's a draw.");
+                if (status == 1 || status == 2) printf("Next move: Player %d\n", status);
+            } else {
+                printf("The Game has ended.\n");
+            }
+        }
+        printf("Awaiting your command: ...\n");
+    }
 }
